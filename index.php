46e8371816c419901d89db3588da4ff184d69cbe
Sawyer Hollenshead <s.hollenshead@gmail.com>
2013-01-21 21:45:02 -0500

    Updates for infinite scroll

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
  * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
  *
@@ -18,27 +18,23 @@
 <div id="wrapper">
 <div id="fullhome">
 <?php if ( have_posts() ): ?>
-<ol>
 <?php while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>								
+		<article>
 			<div class="featured-home"><a href="<?php esc_url( the_permalink() ); ?>"><?php
-			
+
 			the_post_thumbnail( 'custom-post-image' ); // insert "custom size" image
-			
+
 			?></a></div>
 								<div class="cat"><?php the_category(', ') ?></div>
-			
+
 					<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-					
-										<div class="post-info">By <?php the_author(); ?> on <?php the_time('M. j'); ?><b> |</b> <?php comments_popup_link('0 Shares', '1 Share', '% Comments'); ?><b> |</b> Paris</div> 
-			
-		
+
+										<div class="post-info">By <?php the_author(); ?> on <?php the_time('M. j'); ?><b> |</b> <?php comments_popup_link('0 Shares', '1 Share', '% Comments'); ?><b> |</b> Paris</div>
+
+
 				<?php the_content( __( '...', 'twentyten' ) ); ?>
 		</article>
-	</li>
 <?php endwhile; ?>
-</ol>
 <?php else: ?>
 <h2>No posts to display</h2>
 <?php endif; ?>
@@ -52,4 +48,4 @@
 
 </div>
 
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?> 
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
